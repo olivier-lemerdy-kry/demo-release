cf3cd90aad8a3673a535299234c88dc9bf2cf6a4
Olivier Le Merdy <olivier.lemerdy@kry.se>
2023-11-07 11:16:28 +0100

    Controller

--- src/main/java/se/kry/demo/DemoReleaseController.java
+++ src/main/java/se/kry/demo/DemoReleaseController.java
@@ -1,2 +1,13 @@
-package se.kry.demo;public class DemoReleaseController {
+package se.kry.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoReleaseController {
+
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "Hello World!";
+  }
 }
